d32eae999a4d1cefa585c23688579bd50fc4bf3c
Gaurang Patel <gbp301195@gmail.com>
2018-06-20 18:38:56 +0530

    Delete simple_interest.py

--- simple_interest.py
+++
@@ -1,7 +0,0 @@
-principal = int(input ("Enter value for Principal:"))
-years = int(input ("Enter the Number of years:"))
-rate = int(input ("Enter the Rate:"))
-
-Simple_interest = (principal*years*rate)/100
-
-print ("The Interest is: %s" %Simple_interest)
