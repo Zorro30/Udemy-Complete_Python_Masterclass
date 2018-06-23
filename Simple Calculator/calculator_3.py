0c94add43ee5fa12ab7fab5cc23490eab7495cdb
Zorro30 <gbp301195@gmail.com>
2018-06-23 23:24:13 +0530

    Source Code

---
+++ Simple Calculator/calculator_3.py
@@ -0,0 +1,70 @@
+from tkinter import *
+import parser
+
+root = Tk()
+root.title('Calculator')
+
+#get the user input and place it into text field
+i = 0
+def get_variables(num):
+    global i
+    display.insert(i, num)
+    i+=1
+
+def clear_all():
+    display.delete(0,END)
+
+def undo():
+    entire_string = display.get()
+    if len(entire_string):
+        new_string = entire_string[:-1]
+        clear_all()
+        display.insert(0,new_string)
+    else:
+        clear_all()
+        display.insert(0,"ERROR")
+
+#adding the input field
+display = Entry(root)
+display.grid(row=1,columnspan=6,sticky=W+E)
+
+#adding buttons to the calculator 
+
+Button(root,text="1",command = lambda :get_variables(1)).grid(row=2,column=0)
+Button(root,text='2',command = lambda :get_variables(2)).grid(row=2,column=1)
+Button(root,text='3',command = lambda :get_variables(3)).grid(row=2,column=2)
+
+Button(root,text='4',command = lambda :get_variables(4)).grid(row=3,column=0)
+Button(root,text='5',command = lambda :get_variables(5)).grid(row=3,column=1)
+Button(root,text='6',command = lambda :get_variables(6)).grid(row=3,column=2)
+
+Button(root,text='7',command = lambda :get_variables(7)).grid(row=4,column=0)
+Button(root,text='8',command = lambda :get_variables(8)).grid(row=4,column=1)
+Button(root,text='9',command = lambda :get_variables(9)).grid(row=4,column=2)
+
+#adding other buttons to the calculator
+Button(root,text='AC',command = lambda:clear_all()).grid(row=5,column=0)
+Button(root,text='0',command = lambda :get_variables(0)).grid(row=5,column=1)
+Button(root,text='=').grid(row=5,column=2)
+
+Button(root,text='+').grid(row=2,column=3)
+Button(root,text='-').grid(row=3,column=3)
+Button(root,text='*').grid(row=4,column=3)
+Button(root,text='/').grid(row=5,column=3)
+
+#adding new operations
+
+Button(root,text='pi').grid(row=2,column=4)
+Button(root,text='%').grid(row=3,column=4)
+Button(root,text='(').grid(row=4,column=4)
+Button(root,text='exp').grid(row=5,column=4)
+
+Button(root,text='<-',command = lambda :undo()).grid(row=2,column=5)
+Button(root,text='x!').grid(row=3,column=5)
+Button(root,text=')').grid(row=4,column=5)
+Button(root,text="^2").grid(row=5,column=5)
+
+
+
+
+root.mainloop()
