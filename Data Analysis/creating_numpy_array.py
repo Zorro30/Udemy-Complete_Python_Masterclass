cd3da1c8ac93167d9818acb2bf4c32860328212d
Zorro30 <gbp301195@gmail.com>
2018-07-07 16:53:50 +0530

    Source Code

---
+++ Data Analysis/creating_numpy_array.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+#simple numpy array
+a = np.array([1,2,3,4])
+print(a)
+
+#turning a simple array in a multidimensional array
+a = a.reshape(2,2)
+print (a) #it will print two dimensional array.
