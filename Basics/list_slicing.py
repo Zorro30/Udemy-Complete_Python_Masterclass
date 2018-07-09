a7aed479ffd18c13e3f68822ff65ee9d1a5d6750
Zorro30 <gbp301195@gmail.com>
2018-07-09 18:38:33 +0530

    updated code

--- Basics/list_slicing.py
+++ Basics/list_slicing.py
@@ -1,4 +1,7 @@
 numbers = [0, 100, 200, 300, 400, 500, 600]
 
+#printing from a particular index to other using step.
 print (numbers[1:6:2])
-print (numbers[len(numbers)::-1])   
+
+#reversing the list.
+print (numbers[::-1])
--- Basics/quiz7.py
+++ Basics/quiz7.py
@@ -1,8 +1,19 @@
 products = {"toothpaste":80, "Salt":20, "Milk":60, "sugar":45, "Dal":100}
 
-newproduct = input ("Enter the product:")
+print ("You can find prices of these commodities:{}".format([*products]))
+
+newproduct = input ("Enter the product to know the price:")
 
 if newproduct in products:
     print (products.get(newproduct))
 else:
     print ("Product not found.")
+    dec = input ("You wish to add, press yes otherwise no?")
+    if dec == 'yes':
+        name = input ("Enter the comodity:")
+        price = int (input ("Enter the price if you know:"))
+        products[name] = price
+        print ("Your commodity is added for future use. {}".format(products))
+    else:
+        print ("Thank you for your precious time!")
+        exit 
