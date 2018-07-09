e41116ffcc59c7fc432e9b45b8c2817be9659c22
Zorro30 <gbp301195@gmail.com>
2018-07-09 18:39:24 +0530

    updated code

--- Django_Project/mysite/books/urls.py
+++ Django_Project/mysite/books/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'', views.index, name = 'index' ),
+    path('', views.index, name = 'index' ),
     path('<int:book_id>/', views.detail, name = 'detail')
 ]
