cca10ee5ecdc6362ba85900992a0c371d178e6a7
Zorro30 <gbp301195@gmail.com>
2018-07-09 23:43:22 +0530

    Source code

--- Django_Project/mysite/books/urls.py
+++ Django_Project/mysite/books/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'books' #namespaces in Django
+
 urlpatterns = [
     path('', views.index, name = 'index' ),
     path('<int:book_id>/', views.detail, name = 'detail')
--- Django_Project/mysite/books/views.py
+++ Django_Project/mysite/books/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from .models import Books
+#from django.template import loader
+from django.shortcuts import render
+from django.http import Http404
 # Create your views here.
 
 from django.http import HttpResponse
 
 def index(request):
     all_books = Books.objects.all()
-    html = ''
-    for book in all_books:
-        url = "/books/" + str(book.id) + '/'
-        html += '<a href="' + url + '">' +str(book.name)+ ' </a><br>' 
-    return HttpResponse(html)
+    context = {
+        'all_books' : all_books
+    }
+     
+    return render(request,'books/index.html',context)
 
 def detail(request, book_id):
-    return HttpResponse("<h2> Details for Book ID:" +str(book_id)+ "</h2>")
+    try:
+        book = Books.objects.get(id=book_id)
+    except Books.DoesNotExist:
+        raise Http404('This book does not exist')
+    return render(request,'books/detail.html',{'book':book})
