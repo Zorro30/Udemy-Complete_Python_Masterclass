944d3ecb540f43a9b910c46c9f7a001c824f6af3
Zorro30 <gbp301195@gmail.com>
2018-07-09 19:00:37 +0530

    Source Code

--- Django_Project/mysite/books/views.py
+++ Django_Project/mysite/books/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-
+from .models import Books
 # Create your views here.
 
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h>This is the books homepage!</h>")
+    all_books = Books.objects.all()
+    html = ''
+    for book in all_books:
+        url = "/books/" + str(book.id) + '/'
+        html += '<a href="' + url + '">' +str(book.name)+ ' </a><br>' 
+    return HttpResponse(html)
 
 def detail(request, book_id):
     return HttpResponse("<h2> Details for Book ID:" +str(book_id)+ "</h2>")
