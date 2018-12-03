e5ce267f074df02f9bee40b555c145184cdb06ca
Zorro30 <gbp301195@gmail.com>
2018-12-04 00:00:46 +0530

    Code

--- Regular_expression/regex_1.py
+++ Regular_expression/regex_1.py
@@ -1,3 +1,24 @@
+'''
+Identifiers:
+
+\d any number
+\D anything but a number
+\s space
+\S anything but space
+\w any character
+\W anything but caharcter
+. any character, except for a newline
+\b the whitespace around words
+
+Modifiers:
+* match zero or more
++ match one or more
+? match zero or one
+$ match the end of a string
+^ match the beginning of the string
+| either or
+[] range or "variance"
+'''
 import re
 
 myString = 'Send an email from 10-12-2018  or 11/12/2018 this@email.com to test@user.com 34 times'
